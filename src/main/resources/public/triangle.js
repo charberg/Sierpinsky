0162b5898904fb4c0662f1a1079d1c4f0bcf3ddb
Charles Bergeron <charlesbergeron@cmail.carleton.ca>
2017-03-27 22:20:09 -0400

    night before texas, push to laptop

--- src/main/resources/public/triangle.js
+++ src/main/resources/public/triangle.js
@@ -5,6 +5,9 @@ var maxDepth = 5;
 var depth = 0;
 var svg;
 var div;
+//2D array of triangles, where first index is their recursive depth at an offset
+var triangles;
+var zoomCount = 0;
 
 $(document).ready(function () {
     //init();
@@ -27,6 +30,26 @@ $(document).ready(function () {
 
     init();
 
+    enableZoomPan();
+
+});
+
+function init() {
+
+    triangles = create2DArray(5);
+
+    //Calculate triangle line length
+    length = height/Math.sin(60*Math.PI/180)    //Parameter conversion to radians
+
+    t = new Triangle(new Point(0, height), new Point(width, height), new Point(width/2, 0));
+    sketchTriangle(t);
+    //Recursively draw children triangles
+    drawSubTriangles(t, true);
+    attachMouseWheelListener();
+
+}
+
+function enableZoomPan() {
     //Enable zoom/pan
     var test = svgPanZoom('#svg-id', {
         zoomEnabled: true,
@@ -35,44 +58,65 @@ $(document).ready(function () {
         center: true,
         minZoom: 0
     });
-
-
-
-
-});
-
-function init() {
-
-
-
-    //Calculate triangle line length
-    length = height/Math.sin(60*Math.PI/180)    //Parameter conversion to radians
-
-    t = new Triangle(new Point(0, height), new Point(width, height), new Point(width/2, 0));
-    sketchTriangle(t);
-    //Recursively draw children triangles
-    drawSubTriangles(t);
-
 }
+
+function attachMouseWheelListener() {
+    if (div.addEventListener)
+    {
+        // IE9, Chrome, Safari, Opera
+        div.addEventListener("mousewheel", MouseWheelHandler, false);
+        // Firefox
+        div.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+    }
+// IE 6/7/8
+    else
+    {
+        div.attachEvent("onmousewheel", MouseWheelHandler);
+    }
+
+    function MouseWheelHandler(e)
+    {
+        // cross-browser wheel delta
+        var e = window.event || e; // old IE support
+        //Delta +1 -> scrolled up
+        //Delta -1 -> scrolled down
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        console.log(delta);
+
+        zoomCount = zoomCount + delta;
+
+        if(zoomCount==15) {
+
+            for(var i = 0; i < triangles[0].length; i++) {
+                drawSubTriangles(triangles[0][i], false);
+            }
+            enableZoomPan();
+        }
+
+        return false;
+    }
+}
+
 
 function drawLine(p1, p2) {
 
-    var aLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-    aLine.setAttribute('x1', p1.x);
-    aLine.setAttribute('y1', p1.y);
-    aLine.setAttribute('x2', p2.x);
-    aLine.setAttribute('y2', p2.y);
-    aLine.setAttribute('stroke', "black");
-    aLine.setAttribute('stroke-width', 1);
-    svg.appendChild(aLine);
+    var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('x1', p1.x);
+    line.setAttribute('y1', p1.y);
+    line.setAttribute('x2', p2.x);
+    line.setAttribute('y2', p2.y);
+    line.setAttribute('stroke', "black");
+    line.setAttribute('stroke-width', 1);
+    svg.appendChild(line);
 
 }
 
-function drawSubTriangles(t) {
-    console.log(depth);
+//Recursive parameter if you want to draw recursively, or just stop after one level
+function drawSubTriangles(t, recursive) {
     //End condition, bounded by maximum recursion depth
-    if(depth == maxDepth) {
-        console.log("Hit max depth");
+    if(depth == maxDepth && recursive==true) {
+        //Push triangle to depth collection, to track in case zooming in and redrawing
+        triangles[maxDepth-depth].push(t);
         return;
     }
 
@@ -90,9 +134,11 @@ function drawSubTriangles(t) {
     sketchTriangle(midTriangle)
 
     //Recursive call to continue drawing children triangles until max depth
-    drawSubTriangles(new Triangle(t.p1, midPoint1, midPoint3), depth);
-    drawSubTriangles(new Triangle(midPoint3, midPoint2, t.p3), depth);
-    drawSubTriangles(new Triangle(midPoint1, t.p2, midPoint2), depth);
+    if(recursive == true) {
+        drawSubTriangles(new Triangle(t.p1, midPoint1, midPoint3), true);
+        drawSubTriangles(new Triangle(midPoint3, midPoint2, t.p3), true);
+        drawSubTriangles(new Triangle(midPoint1, t.p2, midPoint2), true);
+    }
     depth = depth -1;
 }
 
@@ -102,6 +148,16 @@ function sketchTriangle(t) {
     drawLine(t.p1, t.p2);
     drawLine(t.p2, t.p3);
     drawLine(t.p3, t.p1);
+}
+
+function create2DArray(rows) {
+    var arr = [];
+
+    for (var i=0;i<rows;i++) {
+        arr[i] = [];
+    }
+
+    return arr;
 }
 
 function getCenter(p1, p2) {
